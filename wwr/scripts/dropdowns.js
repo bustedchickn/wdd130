fa98b9157bc7fc8633b41ebd4be501abada318d1
bustedchickn <86179078+bustedchickn@users.noreply.github.com>
2024-03-05 21:57:37 -0700

    finished the page

--- wwr/scripts/dropdowns.js
+++ wwr/scripts/dropdowns.js
@@ -1,5 +1,20 @@
 function myFunction(){
-    document.getElementById("myDropdown").classList.toggle("show");
+    document.getElementById("myDropdown1").classList.toggle("show");
+}
+function myFunction2(){
+  document.getElementById("myDropdown2").classList.toggle("show");
+}
+function myFunction3(){
+  document.getElementById("myDropdown3").classList.toggle("show");
+}
+function myFunction4(){
+  document.getElementById("myDropdown4").classList.toggle("show");
+}
+function myFunction5(){
+  document.getElementById("myDropdown5").classList.toggle("show");
+}
+function myFunction6(){
+  document.getElementById("myDropdown6").classList.toggle("show");
 }
 
 window.onclick = function(event) {
